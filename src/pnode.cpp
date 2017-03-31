92b495a18054250a038aa2ecf829dca3fa775fec
Eric <ericstefan.miele@gmail.com>
2017-03-31 23:16:34 +0200

    almost2

--- src/pnode.cpp
+++ src/pnode.cpp
@@ -1,5 +1,4 @@
-#include "ros/ros.h"
-#include "std_msgs/Float32.h"
+#include <tf/transform_broadcaster.h>
 #include "std_msgs/String.h"
 #include <tf/transform_listener.h>
 #include "sensor_msgs/LaserScan.h"
@@ -8,32 +7,34 @@
 void callback(const sensor_msgs::LaserScan::ConstPtr& input, ros::Publisher pub){
 	
 	tf::TransformListener listener;
-	
 	tf::StampedTransform transform;
 	
-	ros::Time t = (input->header).stamp;
-	
-	//ROS_INFO("%u",t.sec);
-	
-	//if(listener.canTransform("/odom", "/base_laser_link", t, NULL)){
+	if(listener.canTransform("/odom", "/base_laser_link", ros::Time(0), NULL)){
 		
-		listener.lookupTransform("/odom", "/base_laser_link", t, transform);
+		listener.lookupTransform("/odom", "/base_laser_link", ros::Time(0), transform);
+		
+		tf::Vector3 coordinates = transform.getOrigin();
+		
+		float x = coordinates.getX();
+		float y = coordinates.getX();
+		float z = coordinates.getX();
 		
 		std_msgs::String output;
 	
 		std::stringstream ss;
 		
-		ss << "Timestamp: " << transform.stamp_;
+		ss << "Timestamp: " << (input->header).stamp << " - ";
+		ss << "X: " << x << " - ";
+		ss << "Y: " << y << " - ";
+		ss << "X: " << z;
 		
 		output.data = ss.str();
-
-		//ROS_INFO("Min: %f", output.data);
-
+		
 		pub.publish(output);
 
 		ros::spinOnce();
-	//}
-	//else ROS_INFO("Could not transform");
+	}
+	else printf("Unable to transform\n");
 	
 }
 
@@ -42,12 +43,12 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "pnode");
 
 	ros::NodeHandle n;
-	
+
 	ros::Publisher pub = n.advertise<std_msgs::String>("pos_val", 1000);
 
 	ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("base_scan", 1000, boost::bind(callback, _1, pub));
-	
+
 	ros::spin();
 
-  return 0;
+	return 0;
 }
