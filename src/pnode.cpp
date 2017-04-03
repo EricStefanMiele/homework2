00b14e9a8c1e73b7728b25307258925dfe0b885d
Eric <ericstefan.miele@gmail.com>
2017-04-03 13:56:45 +0200

    updated

--- src/pnode.cpp
+++ src/pnode.cpp
@@ -4,49 +4,60 @@
 #include "sensor_msgs/LaserScan.h"
 #include <sstream>
 
-void callback(const sensor_msgs::LaserScan::ConstPtr& input, ros::Publisher pub){
+void callback(const sensor_msgs::LaserScan::ConstPtr& input, ros::Publisher pub, tf::TransformListener* listener){
 	
-	tf::TransformListener listener;
 	tf::StampedTransform transform;
 	
-	if(listener.canTransform("/odom", "/base_laser_link", ros::Time(0), NULL)){
-		
-		listener.lookupTransform("/odom", "/base_laser_link", ros::Time(0), transform);
+	ros::Time stamp = (input->header).stamp;
+	
+	if(listener->canTransform("/odom", "/base_laser_link", stamp, NULL)){
+	
+		//listener->waitForTransform("/odom", "/base_laser_link", stamp, ros::Duration(10));
+			
+		listener->lookupTransform("/odom", "/base_laser_link", stamp, transform);
 		
 		tf::Vector3 coordinates = transform.getOrigin();
-		
+				
 		float x = coordinates.getX();
 		float y = coordinates.getY();
-		float z = coordinates.getZ();
+		//float z = coordinates.getZ();
+		
+		tf::Quaternion q = transform.getRotation();
+		
+		float theta = q.getAngle();
 		
 		std_msgs::String output;
 	
 		std::stringstream ss;
 		
-		ss << "Timestamp: " << (input->header).stamp << " - ";
+		ss << "Timestamp: " << stamp << " - ";
 		ss << "X: " << x << " - ";
 		ss << "Y: " << y << " - ";
-		ss << "X: " << z;
+		ss << "Theta: " << theta;
 		
 		output.data = ss.str();
+		
+		//printf("%s\n",output.data.c_str());
 		
 		pub.publish(output);
 
 		ros::spinOnce();
 	}
-	else printf("Unable to transform\n");
+	else printf("Transformation not available\n");
 	
 }
 
 int main(int argc, char **argv){
 
 	ros::init(argc, argv, "pnode");
+	
+	tf::TransformListener listener;
 
 	ros::NodeHandle n;
 
 	ros::Publisher pub = n.advertise<std_msgs::String>("pos_val", 1000);
 
-	ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("base_scan", 1000, boost::bind(callback, _1, pub));
+	ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("base_scan", 1000, boost::bind(callback, _1, pub, &listener));
 
 	ros::spin();
 
